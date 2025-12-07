d0ea77eda678c810e581ef87cecf9e2286be9801
yungblud <killi8n@gmail.com>
2025-12-08 00:36:31 +0900

    feat(RNApp): RN setup for monorepo

--- apps/RNApp/babel.config.js
+++ apps/RNApp/babel.config.js
@@ -1,3 +1,15 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        alias: {
+          react: '../../node_modules/react',
+          'react-native': '../../node_modules/react-native',
+        },
+      },
+    ],
+  ],
 };
